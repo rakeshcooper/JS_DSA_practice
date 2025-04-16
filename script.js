f501702b3191fc88a63a0e68248a0225dead3162
rakeshcooper <rakeshmurali097@gmail.com>
2025-04-17 01:08:52 +0530

    learning update

--- script.js
+++ script.js
@@ -8,13 +8,43 @@ function abc(){
 
 // abc();
 
-let num = [1,2,3,4]
+let num = [1,2,3,4,5]
 
 let reducedNum = num.reduce((acc,cur,index) => {
-    return cur
+    return acc + cur
 },0)
 
-console.log(reducedNum);
+//console.log(reducedNum);
+
+const words = ['Hello', 'world'];
+
+const sentence = words.reduce((acc, curr) => acc + ' ' + curr);
+
+//console.log(typeof sentence); // Output: "Hello world"
+
+
+
+
+const numbers = [10];
+
+// 'b' is not provided by the array, so it defaults to 20.
+
+
+const [a, b = 20] = numbers;
+
+// console.log(a); // 10
+// console.log(b); // 20
+
+let person = {
+    name: "Cooper",
+    age: 27,
+    section: "howatt",
+    class: "gloot"
+}
+
+let { name, age, ...rest} = person
+console.log(name,age = 25, rest);
+
 
 
 
