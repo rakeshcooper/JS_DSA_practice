6113200c4663fa456cbec625981362e04538657a
rakeshcooper <rakeshmurali097@gmail.com>
2025-04-30 14:24:56 +0530

    update

--- script.js
+++ script.js
@@ -348,6 +348,30 @@ let arr2 = [3,4]
 // console.log(sumpairarr);
 //     const uniqueNumbers = [...new Set(sumpairarr.flat())];
 //     console.log(uniqueNumbers);
+
+
+// find the factorial of the number
+
+function findFactorialNum(n){
+    if(n < 0) return undefined
+    if(n === 0 || n === 1 ) return 1;
+    let result = 1
+    for (let i = 2; i <= n; i++) {
+        result *= i
+    }
+    return result
+}
+
+console.log(findFactorialNum(10));
+
+function findFactorialNumRecursion(n){
+    if(n < 0) return undefined
+    if(n === 0 || n === 1 ) return 1;
+    return n * findFactorialNumRecursion(n - 1)
+}
+
+//console.log(findFactorialNumRecursion(10));
+
     
 
  
