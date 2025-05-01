d9132a0eeca581e4a0961ba7775fce0b50d0bc43
rakeshcooper <rakeshmurali097@gmail.com>
2025-05-01 15:54:28 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 
 
-function isPalindrome(x){
-    return x === +x.toString().split("").reverse().join("")
-}
+// function isPalindrome(x){
+//     return x === +x.toString().split("").reverse().join("")
+// }
 
 
-const results = isPalindrome(121)
+// const results = isPalindrome(121)
 
-console.log(results);
+// console.log(results);
+
+// let dummyArr = []
+// fetch("https://dummyjson.com/todos")
+// .then(res => res.json())
+// .then(data => data.todos.forEach(element => {
+//     dummyArr.push(element)
+// }))
+
+// setTimeout(() => {console.log(dummyArr)
+// },1000)
+
+// let dummyArr = []
+// async function loadApi(){
+//     let res = await fetch("https://dummyjson.com/todos")
+//     let data = await res.json()
+//     dummyArr = [...data.todos]
+//     console.log(dummyArr.length);
+// }
+// loadApi()
+
+
+
+
